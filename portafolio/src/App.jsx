302cb0f94e729979f4a162b20f8c5c283b5236c0
David Palacios <davidpa7710@gmail.com>
2023-01-03 22:38:10 -0600

    🎉  proyect init

---
+++ portafolio/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+     <h1> Hola Mi Nombre Es David Palacios</h1>
+     <h2>Soy FrontEnd Developer</h2>
+     <p>Es es mi portafolio con un peqeño ejempo de lo que e realizado como FrontEnd Developer</p>
+     <button type='button' className='btn-main'> Contactame </button>
+    </div>
+  )
+}
+
+export default App
