a0bf4b2ae9a12dda69e4e05dd8f7b8354c4991ca
David Palacios <davidpa7710@gmail.com>
2023-01-04 19:29:02 -0600

    feat: new backround

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -4,11 +4,13 @@ import './App.css'
 function App() {
   
   return (
-    <div className="App">
+    <div className="App bg">
+      <div className="hero">
      <h1> Hola Mi Nombre Es David Palacios</h1>
      <h2>Soy FrontEnd Developer</h2>
      <p>Es es mi portafolio con un peqeño ejempo de lo que e realizado como FrontEnd Developer</p>
      <button type='button' className='btn-main'> Contactame </button>
+     </div>
     </div>
   )
 }
