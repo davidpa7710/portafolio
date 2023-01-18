acf9a92e52b114dbcfb5184eac0236a3e40f9a9e
David Palacios <davidpa7710@gmail.com>
2023-01-18 17:25:54 -0600

    feat new card features

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -1,16 +1,25 @@
-
+import * as Reveal from 'react-reveal'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  
+
   return (
-    <div className="App bg">
-      <div className="hero">
-     <h1> Hola Mi Nombre Es David Palacios</h1>
-     <h2>Soy FrontEnd Developer</h2>
-     <p>Es es mi portafolio con un peqeño ejempo de lo que e realizado como FrontEnd Developer</p>
-     <button type='button' className='btn-main'> Contactame </button>
-     </div>
+      <div className="App">
+    <Reveal.Fade bottom>
+        <div className="hero">
+          <h1> Hola Mi Nombre Es David Palacios</h1>
+          <h2>Soy FrontEnd Developer</h2>
+          <p>Es es mi portafolio con un peqeño ejempo de lo que e realizado como FrontEnd Developer</p>
+          <button type='button' className='btn-main'> Contactame </button>
+        </div>
+    </Reveal.Fade>
+      <div className='card_slider'>
+    <Reveal.Fade left>
+    <Card />
+    <Card />
+    </Reveal.Fade>
+    </div>
     </div>
   )
 }
---
+++ portafolio/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+
+const Card = () => {
+    return (
+        <div className='card'>
+            <h1>title</h1>
+            <img src="https://www.potters.mx/wp/wp-content/uploads/2016/04/dummy-post-horisontal.jpg" alt="imgdump" width='400px' height='250px' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam reiciendis voluptatibus modi alias vel facilis quidem debitis nobis, tempore, dolore molestiae eligendi similique tempora. Nobis error excepturi est cumque dolorum. Blanditiis, dolorem.</p>
+        </div>
+    )
+}
+
+export default Card
