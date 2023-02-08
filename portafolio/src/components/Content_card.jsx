4ba6008dcf19bc7d79c2e255dc4d36dbaab6eb75
David Palacios <davidpa7710@gmail.com>
2023-02-07 19:13:52 -0600

    feat: content card fixed

--- portafolio/src/components/Content_card.jsx
+++ portafolio/src/components/Content_card.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 import './Content_card.css'
+import * as Reveal from 'react-reveal'
 
 function Content_card() {
     return (
-        <>
-            <div className="min-h-screen w-full bg-gray-300">
-                <div className="max-w-screen-md mx-auto px-10 pt-20">
-                    <div className="bg-white md:h-48 rounded-lg shadow-md flex flex-wrap flex-col-reverse md:flex-col">
-                        <div className="w-full md:w-1/2 p-4">
-                            <img src="https://www.mcdonalds.com/content/dam/usa/nfl/assets/nav/arches-logo_108x108.jpg" alt="" className="w-8" />
-                            <h3 className="text-3xl font-bold">Fast Food &amp; Cola</h3>
-                            <p>Get all your McDonald’s favorites delivered right to your doorstep with McDelivery® on Uber Eats or DoorDash.</p>
-                        </div>
-                        <div className="w-full md:w-1/2 p-4 md:p-0">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png" alt="" className="w-64 mx-auto" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
+        <div className='card_react'>
+            <Reveal.Rotate top left cascade>
+                <img src='../src/assets/react.svg' className='react-logo'></img>
+            </Reveal.Rotate>
+            <Reveal.Zoom cascade top left>
+                <h2>Frontend FrameWork React</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Adipisci ad beatae cupiditate cumque ex quaerat sequi itaquefacere doloribus enim! Fuga ullam voluptatum quaerat cupiditate obcaecati
+                    ipsa culpa deleniti assumenda nam commodi repellat, suscipit itaque
+                    provident veritatis. Aperiam tenetur deleniti atque quos, ab maxime
+                    sapiente voluptatum aspernatur esse rem laboriosam.</p>
+            </Reveal.Zoom>
+        </div>
     )
 }
 
