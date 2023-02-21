a2ed591efffb5c56bd6d36a8348d847bf9482a15
David Palacios <davidpa7710@gmail.com>
2023-02-21 13:42:34 -0600

    fix  images dummies

--- portafolio/src/components/Content_card.jsx
+++ portafolio/src/components/Content_card.jsx
@@ -6,7 +6,7 @@ function Content_card() {
     return (
         <div className='card_react'>
             <Reveal.Rotate top left cascade>
-                <img src='../src/assets/react.svg' className='react-logo'></img>
+                <img src='https://zhargullqfwuquyqmofs.supabase.co/storage/v1/object/sign/imagnes-data/react.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnbmVzLWRhdGEvcmVhY3Quc3ZnIiwiaWF0IjoxNjc3MDA4NDg0LCJleHAiOjE3MDg1NDQ0ODR9.rJ1O1tKMB-XHqu3hOvnkWhYYjilI5mfkypq4D1pah0s&t=2023-02-21T19%3A41%3A24.281Z' className='react-logo'></img>
             </Reveal.Rotate>
             <Reveal.Zoom cascade top left>
                 <h2>Frontend FrameWork React</h2>
--- portafolio/src/components/Proyect_Card.jsx
+++ portafolio/src/components/Proyect_Card.jsx
@@ -4,7 +4,7 @@ import './Proyect_Card.css'
 const Proyect_Card = () => {
     return (
         <div className='website_card'>
-            <img src="../src/assets/Captura de Pantalla 2023-02-20 a la(s) 20.02.06.png" alt="dummy" />
+            <img src="https://zhargullqfwuquyqmofs.supabase.co/storage/v1/object/sign/imagnes-data/Captura%20de%20Pantalla%202023-02-20%20a%20la(s)%2020.02.06.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnbmVzLWRhdGEvQ2FwdHVyYSBkZSBQYW50YWxsYSAyMDIzLTAyLTIwIGEgbGEocykgMjAuMDIuMDYucG5nIiwiaWF0IjoxNjc3MDA4NTI5LCJleHAiOjE3MDg1NDQ1Mjl9.TV1fcx26R93afclb0HgYy3OPYYudiklGoXGnuIZ-wFY&t=2023-02-21T19%3A42%3A09.131Z" alt="dummy" />
             <h2>Pokedex</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur repellendus unde quidem facilis obcaecati ea magni error modi! Animi eum incidunt et dolores obcaecati consequuntur.</p>
             <button className='button-40'>Click Me</button>
